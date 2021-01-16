3a84f9285b389e8ac5f8aefba554cd12892ff168
nesquack <karsen.beck@gmail.com>
2021-01-16 14:49:07 -0600

    asset header align only if there is no padding

--- AssetTools.NET/Standard/AssetsFileFormat/AssetsFile.cs
+++ AssetTools.NET/Standard/AssetsFileFormat/AssetsFile.cs
@@ -194,15 +194,20 @@ namespace AssetsTools.NET
             uint metadataSize = (uint)(writer.Position - 0x13); //0x13 is header - "endianness byte"? (if that's what it even is)
 
             //-for padding only. if all initial data before assetData is more than 0x1000, this is skipped
-            while (writer.Position < 0x1000/*header.offs_firstFile*/)
+            if (writer.Position < 0x1000)
             {
-                writer.Write((byte)0x00);
+                while (writer.Position < 0x1000)
+                {
+                    writer.Write((byte)0x00);
+                }
             }
-
-            if (writer.Position % 16 == 0)
-                writer.Position += 16;
             else
-                writer.Align16();
+            {
+                if (writer.Position % 16 == 0)
+                    writer.Position += 16;
+                else
+                    writer.Align16();
+            }
 
             long offs_firstFile = writer.Position;
 
