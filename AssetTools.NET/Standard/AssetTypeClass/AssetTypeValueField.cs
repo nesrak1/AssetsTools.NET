0d87ae22bed388c3a144a9856d75100de439c510
Igor55 <74247502+Igor55x@users.noreply.github.com>
2021-01-18 22:43:00 +0300

    Add/Remove children improvements

--- AssetTools.NET/Standard/AssetTypeClass/AssetTypeValueField.cs
+++ AssetTools.NET/Standard/AssetTypeClass/AssetTypeValueField.cs
@@ -50,7 +50,17 @@
         public AssetTypeTemplateField GetTemplateField() { return templateField; }
         public AssetTypeValueField[] GetChildrenList() { return children; }
         public void SetChildrenList(AssetTypeValueField[] children) { this.children = children; this.childrenCount = children.Length; }
-
+        public void AddChildren(AssetTypeValueField[] children) { this.children = this.children.Concat(children).ToArray(); }
+        public void AddChildren(AssetTypeValueField children) { this.children = this.children.Append(children).ToArray(); }
+        
+        public void RemoveChildren(AssetTypeValueField[] children)
+        {
+            // Removes an array of similar children
+            this.children = this.children.Except(children).ToArray();
+        }
+        
+        public void RemoveChildren(AssetTypeValueField children) { this.children = this.children.Where(i => i != children).ToArray(); }
+        
         public int GetChildrenCount() { return childrenCount; }
 
         public bool IsDummy()
