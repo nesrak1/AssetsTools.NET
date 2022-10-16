c20bdf855bbcb0a21b95ad8fd26ea5b7fa7cbd5a
nesquack <karsen.beck@gmail.com>
2022-10-15 21:14:26 -0500

    fix comment typo

--- AssetTools.NET/Standard/IO/LZ4BlockStream.cs
+++ AssetTools.NET/Standard/IO/LZ4BlockStream.cs
@@ -214,7 +214,7 @@ namespace AssetsTools.NET
             // we need to do this instead of just reading the first block
             // because if a compressed block ends up being bigger than the
             // actual decompressed size, unity will just use an uncompressed
-            // block instead. so we need to find the first uncomrpessed block.
+            // block instead. so we need to find the first uncompressed block.
             for (int i = 0; i < blockInfos.Length; i++)
             {
                 if (blockInfos[i].GetCompressionType() == 2 || blockInfos[i].GetCompressionType() == 3)
