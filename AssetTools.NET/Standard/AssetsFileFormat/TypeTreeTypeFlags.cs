c265058b7b7e29116a2edf41ff29d19990309a92
nesquack <karsen.beck@gmail.com>
2023-03-02 17:39:17 -0600

    add notes about type tree flags

--- AssetTools.NET/Standard/AssetsFileFormat/TypeTreeTypeFlags.cs
+++ AssetTools.NET/Standard/AssetsFileFormat/TypeTreeTypeFlags.cs
@@ -1,11 +1,29 @@
-﻿namespace AssetsTools.NET
+﻿using System;
+
+namespace AssetsTools.NET
 {
+    [Flags]
     public enum TypeTreeTypeFlags
     {
-        None        = 0b0000,
-        Array       = 0b0001,
-        Ref         = 0b0010,
-        Registry    = 0b0100,
-        ArrayOfRefs = 0b1000
+        None = 0,
+        /// <summary>
+        /// Type tree node is an array.
+        /// </summary>
+        Array = 1,
+        /// <summary>
+        /// Type tree node is a ref type. For example, "managedRefArrayItem" would be an
+        /// array item that is a reference to an object in the registry.
+        /// </summary>
+        Ref = 2,
+        /// <summary>
+        /// Type tree node is a registry. Should just be "ManagedReferencesRegistry references".
+        /// </summary>
+        Registry = 4,
+        /// <summary>
+        /// Type tree node is an array of ref types. This occurs if the SerializeReference was
+        /// added to a list or array instead of just a single field. This is not applied to the
+        /// Array child of the field, just the field itself.
+        /// </summary>
+        ArrayOfRefs = 8
     }
 }
