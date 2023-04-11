60756a50446677e126e087a82550d4a2933f6a2e
nesquack <karsen.beck@gmail.com>
2023-04-11 18:47:54 -0500

    additional info on bundle dir info flags

--- AssetTools.NET/Standard/AssetsBundleFileFormat/AssetBundleDirectoryInfo.cs
+++ AssetTools.NET/Standard/AssetsBundleFileFormat/AssetBundleDirectoryInfo.cs
@@ -12,7 +12,9 @@
         public long DecompressedSize;
         /// <summary>
         /// Flags of this entry. <br/>
-        /// 0x04: File is serialized. Assets files should enable this, and other files like .resS or .resource(s) should disable this.
+        /// 0x01: Entry is a directory. Unknown usage.
+        /// 0x02: Entry is deleted. Unknown usage.
+        /// 0x04: Entry is serialized file. Assets files should enable this, and other files like .resS or .resource(s) should disable this.
         /// </summary>
         public uint Flags;
         /// <summary>
