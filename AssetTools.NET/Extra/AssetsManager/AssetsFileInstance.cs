1341dc0c80f68652f71374093420c798d9e6f5a8
nesquack <karsen.beck@gmail.com>
2023-08-16 10:38:50 -0500

    fix missing bundle dependencies

--- AssetTools.NET/Extra/AssetsManager/AssetsFileInstance.cs
+++ AssetTools.NET/Extra/AssetsManager/AssetsFileInstance.cs
@@ -97,6 +97,10 @@ namespace AssetsTools.NET.Extra
                             {
                                 dependencyCache[depIdx] = am.LoadAssetsFile(bundleLocalAbsPath, true);
                             }
+                            else
+                            {
+                                return null;
+                            }
                         }
                     }
                     else
