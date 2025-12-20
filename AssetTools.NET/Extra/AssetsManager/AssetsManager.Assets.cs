1dbc18cbeb90ae759d458f41ce363a67ce711a6a
Hikaro <35894722+hikarosato@users.noreply.github.com>
2025-12-20 21:24:40 +0200

    Better solution by nesrak1

--- AssetTools.NET/Extra/AssetsManager/AssetsManager.Assets.cs
+++ AssetTools.NET/Extra/AssetsManager/AssetsManager.Assets.cs
@@ -6,7 +6,7 @@ namespace AssetsTools.NET.Extra
     {
         public static string GetFileLookupKey(string path)
         {
-            return Path.GetFullPath(path).ToLower();
+            return Path.GetFileName(path).ToLower();
         }
 
         private void LoadAssetsFileDependencies(AssetsFileInstance fileInst, string path, BundleFileInstance bunInst)
