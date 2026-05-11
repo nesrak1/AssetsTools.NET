d4e5d43c26f17996899d70a8543cfbc64ff595b3
nesquack <karsen.beck@gmail.com>
2026-05-10 22:58:21 -0400

    update cuttlefish build

--- NativeLibs/cuttlefish/linux/lib/cmake/Cuttlefish/cuttlefish_lib-targets-release.cmake
+++ NativeLibs/cuttlefish/linux/lib/cmake/Cuttlefish/cuttlefish_lib-targets-release.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "Cuttlefish::lib" for configuration "Release"
 set_property(TARGET Cuttlefish::lib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(Cuttlefish::lib PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libcuttlefish.so.2.10.0"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libcuttlefish.so.2.10.2"
   IMPORTED_SONAME_RELEASE "libcuttlefish.so.2.10"
   )
 
 list(APPEND _cmake_import_check_targets Cuttlefish::lib )
-list(APPEND _cmake_import_check_files_for_Cuttlefish::lib "${_IMPORT_PREFIX}/lib/libcuttlefish.so.2.10.0" )
+list(APPEND _cmake_import_check_files_for_Cuttlefish::lib "${_IMPORT_PREFIX}/lib/libcuttlefish.so.2.10.2" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
