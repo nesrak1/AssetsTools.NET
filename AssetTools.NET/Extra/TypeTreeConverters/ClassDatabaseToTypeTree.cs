c6b77567b371b1ddaba5b2d3ed45534e87dea3b4
nesquack <karsen.beck@gmail.com>
2026-07-31 23:49:18 -0400

    initialize a typeblob for C2TT as well

--- AssetTools.NET/Extra/TypeTreeConverters/ClassDatabaseToTypeTree.cs
+++ AssetTools.NET/Extra/TypeTreeConverters/ClassDatabaseToTypeTree.cs
@@ -50,7 +50,8 @@ namespace AssetsTools.NET.Extra
                 IsStrippedType = false,
                 ScriptIdHash = Hash128.NewBlankHash(),
                 TypeHash = Hash128.NewBlankHash(),
-                TypeDependencies = new int[0]
+                TypeDependencies = new int[0],
+                TypeBlob = new TypeTreeBlob(),
             };
 
             cldbFile = classes;
